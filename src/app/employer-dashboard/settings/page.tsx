d295d1df3854d42b5856fbca3c4c91af23fd7a34
thapa technical <vbthapa55@gmail.com>
2025-11-21 12:37:02 +0530

    employers setting page done

---
+++ src/app/employer-dashboard/settings/page.tsx
@@ -0,0 +1,10 @@
+import EmployerSettingsForm from "@/features/employers/components/employer-setting-form";
+const EmployerSettings = () => {
+  return (
+    <div>
+      <EmployerSettingsForm />
+    </div>
+  );
+};
+
+export default EmployerSettings;
